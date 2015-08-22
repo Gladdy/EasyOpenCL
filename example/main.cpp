73f4b461ca03e06c4379acea8a4b6468554f448f
Martijn Bakker <martijn.simon.bakker@hotmail.com>
2015-08-22 23:30:12 +0200

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -7,18 +7,18 @@
 
 int main() {
 
-	try {
-		EasyOpenCL<int> framework (SHOW_DEBUG);
-		framework.loadKernel("simplekernel.cl");
+  try {
+    EasyOpenCL<int> framework (SHOW_DEBUG);
+    framework.loadKernel("simplekernel.cl");
 
-		framework.setInputBuffer(0, std::vector<int> {1, 2, 3, 4, 5});
-		framework.setSingleValue(1, 10);
-		framework.setOutputBuffer(2);
+    framework.setInputBuffer(0, std::vector<int> {1, 2, 3, 4, 5});
+    framework.setSingleValue(1, 10);
+    framework.setOutputBuffer(2);
 
     framework.runKernel();
-		framework.showAllValues();
-	}
-	catch (std::exception& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
+    framework.showAllValues();
+  }
+  catch (std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
 }
